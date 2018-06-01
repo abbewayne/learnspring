5a5d013fb5193142925bfe0c8684bbda05f9247c
abbewayne <loweyuyu@gmail.com>
2018-06-01 12:26:01 -0700

    bean scope or singleton

--- spring-demo-one/src/com/luv2code/springdemo/BeanScopeDemoApp.java
+++ spring-demo-one/src/com/luv2code/springdemo/BeanScopeDemoApp.java
@@ -14,6 +14,20 @@ public class BeanScopeDemoApp {
 		Coach theCoach = context.getBean("myCoach", Coach.class);
 		
 		Coach alphaCoach = context.getBean("myCoach", Coach.class);
+		
+		// check if they are the same
+		boolean result = (theCoach == alphaCoach);
+		
+		// print out the results
+		System.out.println("\nPointing to the same object: " + result);
+		
+		System.out.println("\nMemory location for theCoach: " + theCoach);
+		
+		System.out.println("\nMemory location for alphaCoach: " + alphaCoach + "\n");
+		
+		// close the context
+		context.close();
+		
 	}
 
 }
