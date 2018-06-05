f68a7e817972b58c3bef63ab877f00abfbbeba12
abbewayne <loweyuyu@gmail.com>
2018-06-04 17:53:32 -0700

    default beanID

--- Spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
+++ Spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -2,7 +2,7 @@ package com.luv2code.springdemo;
 
 import org.springframework.stereotype.Component;
 
-@Component("thatSillyCOach")
+@Component
 public class TennisCoach implements Coach {
 
 	@Override
