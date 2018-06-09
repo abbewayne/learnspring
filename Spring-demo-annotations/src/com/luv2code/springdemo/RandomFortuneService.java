c19b39dc12001020e1a54b3f3a5deb8967c22ed5
abbewayne <loweyuyu@gmail.com>
2018-06-08 18:36:21 -0700

    scope type with annotations

--- Spring-demo-annotations/src/com/luv2code/springdemo/RandomFortuneService.java
+++ Spring-demo-annotations/src/com/luv2code/springdemo/RandomFortuneService.java
@@ -2,17 +2,43 @@ package com.luv2code.springdemo;
 
 import java.util.Random;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RandomFortuneService implements FortuneService {
 
 	// create an array of strings
+	private String[] data = new String[3];
+	
+	@Value("${lucky1}")
+	private String luck1;
+	
+	@Value("${lucky2}")
+	private String luck2;
+	
+	@Value("${lucky3}")
+	private String luck3;
+	
+	@PostConstruct
+	public void setupMyData() {
+		
+		data[0] = luck1;
+		data[1] = luck2;
+		data[2] = luck3;
+	}
+	
+	//private String[] data = {luck1, luck2, luck3};
+	
+	
+	/*
 	private String[] data = {
 			"Beware",
 			"Diligence",
 			"Journey"
-	};
+	};*/
 	
 	// create a random number generator
 	private Random myRandom = new Random();
@@ -20,6 +46,7 @@ public class RandomFortuneService implements FortuneService {
 	@Override
 	public String getFortune() {
 		// pick a  random string from the array
+		setupMyData();
 		int index = myRandom.nextInt(data.length);
 		
 		String theFortune = data[index];
