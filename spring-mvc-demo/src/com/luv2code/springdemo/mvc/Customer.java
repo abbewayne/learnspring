f624634574537d756c71e3d255ca0ef6cea04d44
abbewayne <loweyuyu@gmail.com>
2018-07-05 18:11:20 -0700

    validate number range

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/Customer.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/Customer.java
@@ -1,5 +1,7 @@
 package com.luv2code.springdemo.mvc;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -9,9 +11,14 @@ public class Customer {
 		System.out.println("call Customer constructor");
 	}
 	
+	@NotNull(message="is required")
+	@Min(value=0, message="must be greater than or equal to zero")
+	@Max(value=10, message="must be less than or equal to Ten")
+	private String freePasses;
+	
 	private String firstName;
 	
-	@NotNull()
+	@NotNull(message="is required")
 	@Size(min=1, message="is required")
 	private String lastName;
 
@@ -29,6 +36,14 @@ public class Customer {
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+
+	public String getFreePasses() {
+		return freePasses;
+	}
+
+	public void setFreePasses(String freePasses) {
+		this.freePasses = freePasses;
 	}
 	
 	
