105158e294c2b278cdd3623e3347b3433ec822d9
abbewayne <loweyuyu@gmail.com>
2018-07-10 18:02:06 -0700

    regexp valida

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/Customer.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/Customer.java
@@ -3,6 +3,7 @@ package com.luv2code.springdemo.mvc;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Customer {
@@ -14,13 +15,24 @@ public class Customer {
 	@NotNull(message="is required")
 	@Min(value=0, message="must be greater than or equal to zero")
 	@Max(value=10, message="must be less than or equal to Ten")
-	private String freePasses;
+	private Integer freePasses;
 	
 	private String firstName;
 	
 	@NotNull(message="is required")
 	@Size(min=1, message="is required")
 	private String lastName;
+	
+	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
+	private String postalCode;
+	
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
 
 	public String getFirstName() {
 		return firstName;
@@ -38,11 +50,11 @@ public class Customer {
 		this.lastName = lastName;
 	}
 
-	public String getFreePasses() {
+	public Integer getFreePasses() {
 		return freePasses;
 	}
 
-	public void setFreePasses(String freePasses) {
+	public void setFreePasses(Integer freePasses) {
 		this.freePasses = freePasses;
 	}
 	
